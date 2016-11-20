f50dad8e8ed6110d31df9074d7f0d23b46e9c937
RafaelMq <rafael_mq2@hotmail.com>
2016-11-20 19:47:47 -0300

    + Inseminacao por tarefa

--- src/main/java/com/magossi/apisimb/resources/tarefa/TarefaResource.java
+++ src/main/java/com/magossi/apisimb/resources/tarefa/TarefaResource.java
@@ -1,6 +1,7 @@
 package com.magossi.apisimb.resources.tarefa;
 
 import com.magossi.apisimb.domain.bovino.Bovino;
+import com.magossi.apisimb.domain.matriz.Inseminacao;
 import com.magossi.apisimb.domain.tarefa.Tarefa;
 import com.magossi.apisimb.service.tarefa.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.constraints.AssertFalse;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -27,6 +31,25 @@ public class TarefaResource {
     @RequestMapping(method =  RequestMethod.POST)
     public ResponseEntity<Void> salvar(@RequestBody Tarefa tarefa){
         tarefa = tarefaService.salvar(tarefa);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(tarefa.getIdTarefa()).toUri();
+
+        return ResponseEntity.created(uri).build();
+    }
+
+    @RequestMapping(value = "/{id}/inseminacao", method =  RequestMethod.POST)
+    public ResponseEntity<Void> salvarInseminacaoTarefa(@PathVariable("id") Long id, @RequestBody Inseminacao inseminacao){
+        Tarefa tarefa = tarefaService.buscarId(id);
+        if (tarefa.getBovinoMatriz().getFichaMatriz().getInseminacao().isEmpty()){
+            List<Inseminacao> inseminacaos = new ArrayList<>();
+            tarefa.getBovinoMatriz().getFichaMatriz().setInseminacao(inseminacaos);
+        }
+        tarefa.setStatusDaTarefa(true);
+        tarefa.setDataConclusao(new Date());
+        tarefa.getBovinoMatriz().getFichaMatriz().getInseminacao().add(inseminacao);
+        tarefa = tarefaService.atualizar(tarefa);
+
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
                 .path("/{id}").buildAndExpand(tarefa.getIdTarefa()).toUri();
--- src/main/java/com/magossi/apisimb/service/tarefa/TarefaService.java
+++ src/main/java/com/magossi/apisimb/service/tarefa/TarefaService.java
@@ -3,6 +3,7 @@ package com.magossi.apisimb.service.tarefa;
 import com.magossi.apisimb.domain.tarefa.Tarefa;
 import com.magossi.apisimb.repository.tarefa.TarefaRepository;
 import com.magossi.apisimb.service.exceptions.BovinoExistenteException;
+import com.magossi.apisimb.service.exceptions.BovinoNaoEncontradoException;
 import com.magossi.apisimb.service.exceptions.BovinoNaoExistenteException;
 import com.magossi.apisimb.service.exceptions.EccNaoEncontradoException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,6 +32,26 @@ public class TarefaService {
             }
         }
         return tarefaRepository.save(tarefa);
+    }
+
+    public Tarefa atualizar(Tarefa tarefa){
+        if(tarefa.getIdTarefa() != null){
+
+            Tarefa t = tarefaRepository.findOne(tarefa.getIdTarefa());
+            if(t == null){
+                throw new BovinoExistenteException("Tarefa Nao Existe");
+            }
+        }
+        return tarefaRepository.save(tarefa);
+    }
+
+    public Tarefa buscarId(Long id){
+        Tarefa tarefa = tarefaRepository.findOne(id);
+
+        if(tarefa==null){
+            throw new BovinoNaoEncontradoException("Tarefa não Encontrada");
+        }
+        return tarefa;
     }
 
     public List<Tarefa> buscarTodos(){
